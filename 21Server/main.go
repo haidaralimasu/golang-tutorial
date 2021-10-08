d1badf0fdb2a088e761b0b53b990bfb9ac87ef5b
Haidar1528 <haidaralimasuhaidarali@gmail.com>
2021-10-08 09:05:06 +0530

    initial commit

--- 21Server/main.go
+++ 21Server/main.go
@@ -4,43 +4,84 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 func main() {
-	fmt.Println("GET request in golang")
-	PerformGetRequest()
+	fmt.Println("Welcome to web verb video - LCO")
+	//PerformGetRequest()
+	PerformPostJsonRequest()
+	// PerformPostFormRequest()
 }
 
 func PerformGetRequest() {
-	const myurl string = "http://localhost:3000"
+	const myurl = "http://localhost:8000/get"
 
 	response, err := http.Get(myurl)
-
 	if err != nil {
 		panic(err)
 	}
 
 	defer response.Body.Close()
 
-	fmt.Println("status : ", response.StatusCode)
-	fmt.Println("length ", response.ContentLength)
+	fmt.Println("Status code: ", response.StatusCode)
+	fmt.Println("Content length is: ", response.ContentLength)
 
 	var responseString strings.Builder
-	content, err := ioutil.ReadAll(response.Body)
+	content, _ := ioutil.ReadAll(response.Body)
+	byteCount, _ := responseString.Write(content)
+
+	fmt.Println("ByteCount is: ", byteCount)
+	fmt.Println(responseString.String())
+
+	//fmt.Println(content)
+	//fmt.Println(string(content))
+}
+
+func PerformPostJsonRequest() {
+	const myurl = "http://localhost:3000/post"
+
+	//fake json payload
+
+	requestBody := strings.NewReader(`
+		{
+			"coursename":"Let's go with golang",
+			"price": 0,
+			"platform":"learnCodeOnline.in"
+		}
+	`)
+
+	response, err := http.Post(myurl, "application/json", requestBody)
 
 	if err != nil {
 		panic(err)
 	}
+	defer response.Body.Close()
 
-	byteCount, _ := responseString.Write(content)
+	content, _ := ioutil.ReadAll(response.Body)
 
-	fmt.Println(byteCount)
-	fmt.Println(responseString.String())
+	fmt.Println(string(content))
+}
 
+func PerformPostFormRequest() {
+	const myurl = "http://localhost:8000/postform"
 
+	//formdata
 
+	data := url.Values{}
+	data.Add("firstname", "hitesh")
+	data.Add("lastname", "choudhary")
+	data.Add("email", "hitesh@go.dev")
 
-	// fmt.Println(string(content))
-	// fmt.Println(content)
+	response, err := http.PostForm(myurl, data)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
+
 }
