7f5b53dbb57f47da51889079bcd410def6c23183
Haidar1528 <haidaralimasuhaidarali@gmail.com>
2021-11-02 07:55:02 +0530

    initial commit

--- 24Api/main.go
+++ 24Api/main.go
@@ -91,3 +91,24 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 	return
 
 }
+
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from request
+	params := mux.Vars(r)
+
+	// loop, id, remove, add
+	for index, course := range courses {
+		if courses.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseId = params["id"]
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+}
